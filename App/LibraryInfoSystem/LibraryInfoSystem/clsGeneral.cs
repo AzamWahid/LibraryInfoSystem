4298f5ef7bf65293530f9c208ea392356c3fdbe3
azamwahid <azamwahid@outlook.com>
2023-10-09 23:55:40 +0500

    initial commit

---
+++ App/LibraryInfoSystem/LibraryInfoSystem/clsGeneral.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryInfoSystem
+{
+    internal class clsGeneral
+    {
+        public static string getConnectionString()
+        {
+           // string ConnectionString = @"Data Source=azam-lt\sql19;Initial Catalog=bookstore;User ID=sa;Password=sasasa-1;MultipleActiveResultSets=True";
+            string connectionString = ConfigurationManager.ConnectionStrings["Connection_String"].ConnectionString;
+
+            return connectionString;
+        }
+     
+    }
+}
